33fc52edd4a1b982120279fb954dcb4358a1de7a
lokendra.k <lokendra.k@cisinlabs.com>
2019-01-30 14:33:51 +0530

    Fixed the item loading problem on edit item page.

--- app/webroot/js/inventory.js
+++ app/webroot/js/inventory.js
@@ -1,5 +1,6 @@
 $(function(){
 	var test_server = "http://acesys.ace1.ca/acetest/acesys-2.0/index.php/";
+	// var live_server = "http://acesys.ace1.ca/index.php/";
 	var live_server = "http://acesys.ace1.ca/index.php/";
 	
 	var l = $(location).attr('href');
@@ -7,7 +8,7 @@ $(function(){
 	if(l.indexOf("acesys-2.0") != -1) G_URL = test_server;
 	else G_URL = live_server;
 
-	G_URL = "http://acecare.ca/acesys/index.php/"
+	G_URL = "/acesys/index.php/"
 
 	//tabs
 	$('#tabs').tabs();
